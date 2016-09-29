087a6ae2189e6752006f222323e5ef50975be922
quet-romain <Quet.romain@gmail.com>
2016-09-29 17:10:25 +0200

    update

--- scripts/my-jquery.js
+++ scripts/my-jquery.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 	
-	$('#competences').hide();
+	$('.text1').hide();
+	$('#titre1').click(function(){
+		$('.text1').fadeIn();
+		$('.text1').hide();
+	});
 
+	$('.text2').hide();
+	$('#titre2').click(function(){
+		$('.text2').fadeIn();
+		$('.text2').hide();
+	});
 
 
 
