5c10d0ecdd483788e7a31480fd77d062635ee952
quet-romain <Quet.romain@gmail.com>
2016-10-05 17:13:56 +0200

    update php

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+	<head>
+		<meta charset="utf-8">
+		<title>site Quet Romain</title>
+		<link rel="stylesheet" type="text/css" href="./style.css">
+		<script type="text/javascript" src="scripts/jquery-3.1.1.js"></script>
+		<script type="text/javascript" src="scripts/script.js"></script>
+	</head>
+	<body>
+		<!--audio autoplay="" loop=""><source src="http://gamestrem.rainwave.cc:8000/game.ogg" type="audio/mpeg"></audio-->
+		<main>
+			<header>
+					<nav>
+						<ul>
+							<li><a href="https://www.facebook.com/romain.quet.9" target="_blank"><img src="./Ressource/ico-facebook.png"></a></li>
+							<li><a href="https://twitter.com/Lyorom" target="_blank" ><img src="./Ressource/twitter.png"></a></li>
+							<li><a href="https://plus.google.com/u/0/118351837694085572798" target="_blank" ><img src="./Ressource/gplus_icon.png"></a></li>
+							<li><a href="https://github.com/quet-romain" target="_blank" ><img src="./Ressource/logo-icon-github.png"></a></li>
+
+						</ul>
+					</nav>
+
+						<img class="photoR" src="./Ressource/photo.png" alt="Photo Romain Q">
+						<p id="name">Romain Quet</p>
+
+					<h1>Technicien Signalétique</h1>
+
+			</header>
+
+			<section class="allsection" id="competences">
+
+					<h3>Compétences profesionelles</h3>
+					<table>
+
+					<tr>
+						<th colspan="2">Informatiques</th>
+						<th colspan="2">Atelier</th>
+					</tr>
+
+					<tr>
+						<td>Illustrator</td>
+						<td>Maitrise</td>
+						<td>Traçage</td>
+						<td>Maitrise</td>
+					</tr>
+
+					<tr>
+						<td>Photoshop</td>
+						<td>Maitrise</td>
+						<td>Painture</td>
+						<td>Aquisition</td>
+					</tr>
+
+					<tr>
+						<td>SketchUp</td>
+						<td>Aquisition</td>
+						<td>pose adhesif</td>
+						<td>Maitrise</td>
+					</tr>
+				</table>
+			</section>
+
+			<section class="allsection" id="experiences">
+
+				<h3>Expériences profesionelles</h3>
+							
+				<p>2008-2011: 4 Stage de decouverte au collége</p>
+				<p>2012-2013: Stage a pub-eprint a Uzes (6 semains)</br>
+				2013-2014: Stage a publi déco a Alés (6 semains)</br>
+				2014-2015: Stage a Diffusion a Alés (6 semains)</br>
+				2015-2016: Stage a Atelier 7 a Alés (6 semains)</br></p>
+
+			</section>
+
+			<section class="allsection" id="contact">
+
+				<form action="contact.php" method="post">
+					<label for="nom">Votre nom</label>
+					<input type="text" id="nom" name="nom" required>
+					<label for="adressemail">votre mail</label>
+					<input type="email" id="adressemail" name="adressemail" required>
+					<label for="message">Votre texte</label>
+					<textarea id="message" type="text" name="message" required></textarea>
+					<button type="submit">Envoyer</button>
+				</form>
+
+			</section>
+
+			<footer>
+				<nav>
+					<ul>
+						<li><a id="buttonCompetences" href="#">Competences |</a></li>
+						<li><a id="buttonExperiences" href="#">Experiences |</a></li>
+						<li><a id="buttonFormation" href="#">Formation |</a></li>
+						<li><a id="buttonContact" href="#">Contact</a></li>
+					</ul>
+
+				</nav>
+			</footer>
+		</main>
+	</body>
+
+</html>
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		<!--<section>	
+						
+				<div class="allsection" id="formation">
+					<h3>Formation</h3>
+					<ul>
+						<li>2008-2011: Collége Lou Redoune uzes</li>
+							<ul>
+								<li>CFG</li>
+								<li>EUROPASS</li>
+								</ul>
+						<li>2012-2014: Lycée F. Mistral Nîmes</li>
+							<ul>
+								<li>CAP Signalétique,Enseigne,Décor</il>
+							</ul>
+						<li>2015-2016: Lycée F. Mistral Nîmes</li>
+							<ul>
+								<li>BMA Brevet des Métiers d'Art</br>
+								OPTION Graphique, Décor</li>
+							</ul>
+
+				</div>
+					
+				<div class="allsection" id="hobbi">
+					<h3>Centres d'intérêts</h3>
+					<p>J'aime</p>
+					<ul>
+						<li>L'informatique</li>
+							<ul>
+								<li>La programmation</li>
+								<li>Réparer les ordinateur</li>
+								<li>Surfer sur le web</li>
+							</ul>
+						<li>Les series de sience fiction</li>
+						<li>La natation</li>
+						<li>La nature</li>
+					</ul>
+				</div>
+		</section>-->
