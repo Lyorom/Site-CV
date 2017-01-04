aa3770c713acfc787e9c461e2ff23204088ecb51
quet-romain <quet.romain@gmail.com>
2017-01-04 15:45:24 +0100

    modif locate

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,4 @@
 $(document).ready(function(){
 
-    $(".button-collapse").sideNav();
-    
-    $('.allsection').hide();
-
-	$('#button4').click(function(){
-		$(".allsection").hide();
-		$("#contact").fadeIn("slow");
-	});
-    
-    $('#button1').click(function(){
-		$(".allsection").hide();
-		$("#competences").fadeIn("slow");
-	});
     
 });
